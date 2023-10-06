1524e57395afde188b7719d7f3b537f89748a596
inyeong-kang <tomatorkd@naver.com>
2023-10-07 06:29:28 +0900

    chore: img 태그 next/Image 태그로 변경

--- src/components/common/UpButton/index.tsx
+++ src/components/common/UpButton/index.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import chevronUp from '@/assets/chevron_up_primary.svg';
 
 import * as S from './style';
+import Image from 'next/image';
 
 interface UpButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export default function UpButton({ ...rest }: UpButtonProps) {
   return (
     <S.Button {...rest}>
-      <img src={chevronUp} alt='페이지 최상단으로 스크롤 올리기' />
+      <Image src={chevronUp} alt='페이지 최상단으로 스크롤 올리기' />
     </S.Button>
   );
 }
