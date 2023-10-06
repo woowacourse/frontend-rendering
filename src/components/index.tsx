0ea3753816b57cf2fe943cff9ca2d85f2dffc1b3
inyeong-kang <tomatorkd@naver.com>
2023-10-07 04:32:28 +0900

    feat: 메인 홈페이지에서 사용되는 공용 컴포넌트 추가

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as Drawer } from './common/Drawer';
+export { default as NarrowMainHeader } from './common/NarrowMainHeader';
+export { default as Skeleton } from './common/Skeleton';
+export { default as UpButton } from './common/UpButton';
+export { default as PostList } from './post/PostList';
