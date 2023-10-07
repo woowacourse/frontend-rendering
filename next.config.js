454871fa5f923ad968902910ae4cfdb9c6e264eb
inyeong-kang <tomatorkd@naver.com>
2023-10-07 14:15:12 +0900

    chore: 환경변수 세팅

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  env: {
+    VOTOGETHER_BASE_URL: process.env.VOTOGETHER_BASE_URL,
+    VOTOGETHER_MOCKING_URL: process.env.VOTOGETHER_MOCKING_URL,
+    VOTOGETHER_REST_API_KEY: process.env.VOTOGETHER_REST_API_KEY,
+    VOTOGETHER_SERVER_REDIRECT_KEY: process.env.VOTOGETHER_SERVER_REDIRECT_KEY,
+  },
 };
 
 module.exports = nextConfig;
