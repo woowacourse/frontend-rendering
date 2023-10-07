a72c0e1edf99757a7bffd0f945bc9c632aae70fb
Q Kim <qoli0703@gmail.com>
2023-10-07 19:59:32 +0900

    feat: Image 컴포넌트 로딩 방식 개선

---
+++ src/models/ImageSourceList.ts
@@ -0,0 +1,44 @@
+import { getResizedImageUrl, isServerStaticData } from 'utils/image';
+import sadpiumiPng from 'assets/sadpiumi-imageFail.png';
+
+const failedImageUrls = new Set<string>();
+
+class ImageSourceList {
+  private fallbackImages: string[] = [sadpiumiPng.src];
+  /**
+   * @param src img src
+   * @param imageSize 사용할 크기 (픽셀, px)
+   */
+  constructor(src: string, imageSize: number) {
+    if (typeof src !== 'string') {
+      this.fallbackImages.push(src);
+      return;
+    }
+
+    if (!failedImageUrls.has(src)) this.fallbackImages.push(src);
+
+    if (isServerStaticData(src)) {
+      const pngUrl = getResizedImageUrl(src, imageSize, 'png');
+      const webpUrl = getResizedImageUrl(src, imageSize, 'webp');
+
+      if (!failedImageUrls.has(pngUrl)) this.fallbackImages.push(pngUrl);
+      if (!failedImageUrls.has(webpUrl)) this.fallbackImages.push(webpUrl);
+    }
+
+    console.log(this.fallbackImages);
+  }
+
+  getCurrent() {
+    return this.fallbackImages[this.fallbackImages.length - 1];
+  }
+
+  getNext() {
+    if (this.fallbackImages.length > 1) {
+      const failed = this.fallbackImages.pop();
+      if (typeof failed === 'string') failedImageUrls.add(failed);
+    }
+    return this.getCurrent();
+  }
+}
+
+export default ImageSourceList;
