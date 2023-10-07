5461c79b4df916fe9246ea8ad719adc7ea923067
inyeong-kang <tomatorkd@naver.com>
2023-10-07 14:18:41 +0900

    feat: ErrorBoundary 구현

---
+++ src/app/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  errorMessage: string;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, errorMessage: '' };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, errorMessage: error.message };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>오류가 발생했습니다. 다시 시도해주세요!</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import * as S from './style';
 import Layout from '@/components/common/Layout';
 import Drawer from '@/components/common/Drawer';
 import { useDrawer } from '@/hooks';
+import ErrorBoundary from './ErrorBoundary';
 
 export default function HomePage() {
   const { drawerRef, closeDrawer, openDrawer } = useDrawer('left');
@@ -29,6 +30,11 @@ export default function HomePage() {
             여기에서 대시보드를 볼 수 있어요.
           </Drawer>
         </S.DrawerWrapper>
+        <ErrorBoundary>
+          <Suspense fallback={<Skeleton isLarge />}>
+            <PostList />
+          </Suspense>
+        </ErrorBoundary>
         <S.ButtonContainer>
           <UpButton onClick={smoothScrollToTop} />
         </S.ButtonContainer>
