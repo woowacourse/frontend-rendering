58033b670c8fa26c432bff4838a738725884cf22
kyw0716 <kyw0716@naver.com>
2023-10-08 14:45:06 +0900

    feat: 구글 지도를 렌더링하기 위한 store 추가

---
+++ carffeine/src/constants/googleMaps.ts
@@ -0,0 +1,8 @@
+export const MIN_ZOOM_LEVEL = 8;
+export const MAX_ZOOM_LEVEL = 20;
+export const INITIAL_ZOOM_LEVEL = 16;
+
+export const DEFAULT_CENTER = {
+  lat: 37.5056102333107,
+  lng: 127.05081496722168,
+} as const;
