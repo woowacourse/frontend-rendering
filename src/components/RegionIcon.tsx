68e651ab6a91fe629a6ed37a80d019a23609c7b1
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-08 19:49:02 +0900

    feat: 지역 아이콘 컴포넌트 UI 구현

---
+++ src/components/RegionIcon.tsx
@@ -0,0 +1,16 @@
+const RegionIcon = () => {
+  return (
+    <div className='w-16 h-16 bg-red-100 rounded-full overflow-hidden relative'>
+      <img
+        className='w-full h-full bg-cover'
+        src='https://www.celuveat.com/images-data/regions/seongsu.jpeg'
+      />
+      <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
+        <div className='absolute w-full h-full bg-black opacity-30' />
+        <span className='text-sm text-white z-10'>성수</span>
+      </div>
+    </div>
+  );
+};
+
+export default RegionIcon;
