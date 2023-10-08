d4f7341625c6baeb6cfe22ffa0b52f1e348b7e2f
Q Kim <qoli0703@gmail.com>
2023-10-08 16:05:16 +0900

    refactor: nextjs로 이사 완료

--- src/app/dict/page.tsx
+++ src/app/dict/page.tsx
@@ -1,13 +1,15 @@
+'use client';
+
 import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import Navbar from 'components/@common/Navbar';
+import { useSearchParams } from 'next/navigation';
+// import Navbar from 'components/@common/Navbar';
 import SearchBox from 'components/search/SearchBox';
 import SearchResults from 'components/search/SearchResults';
 import { Title, Wrapper } from './DictionaryPlantSearch.style';
 import useDictionaryNavigate from 'hooks/dictionaryPlant/useDictionaryPlantNavigate';
 
 const DictionarySearch = () => {
-  const [params] = useSearchParams();
+  const params = useSearchParams();
   const search = params.get('search') ?? '';
 
   const { goToProperDictionaryPlantPage, goToDictionaryPlantDetailPage } = useDictionaryNavigate();
@@ -26,7 +28,7 @@ const DictionarySearch = () => {
         <Title>&quot;{search}&quot; 검색 결과</Title>
         <SearchResults plantName={search} />
       </Wrapper>
-      <Navbar />
+      {/* <Navbar /> */}
     </>
   );
 };
