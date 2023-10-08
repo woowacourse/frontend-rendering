715ccc2fa6414963938589bf19341c820364f038
Leejin-Yang <ejin1996@gmail.com>
2023-10-09 04:01:17 +0900

    feat: 내비게이션바 현재 경로 스타일 수정

--- src/components/common/NavigationBar/NavigationBar.tsx
+++ src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import cx from 'classnames';
+
 import { SvgIcon } from '..';
 
 import { NAVIGATION_MENU } from '@/constants';
@@ -6,17 +11,27 @@ import { colors } from '@/styles/theme';
 import styles from './navigationBar.module.css';
 
 const NavigationBar = () => {
+  const currentPathname = usePathname();
+  const isCurrentPathname = (pathname: string) => currentPathname === pathname;
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.menuList}>
-        {NAVIGATION_MENU.map(({ variant, name }) => {
-          return (
-            <li key={variant} className={styles.menuItem}>
-              <SvgIcon variant={variant} color={colors.gray3} />
-              <p className={styles.menuText}>{name}</p>
-            </li>
-          );
-        })}
+        {NAVIGATION_MENU.map(({ variant, name, pathname }) => (
+          <li key={variant} className={styles.menuItem}>
+            <SvgIcon
+              variant={variant}
+              color={isCurrentPathname(pathname) ? colors.gray5 : colors.gray3}
+            />
+            <p
+              className={cx(styles.menuText, {
+                [styles.currentPath]: isCurrentPathname(pathname),
+              })}
+            >
+              {name}
+            </p>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,27 +21,39 @@ export const SVG_ICON_VARIANTS = [
 ] as const;
 export type SvgIconVariant = (typeof SVG_ICON_VARIANTS)[number];
 
+const PATH = {
+  HOME: '/',
+  LIST: '/products/food',
+  RECIPE: '/recipes',
+  MEMBER: '/members',
+};
+
 export interface NavigationMenu {
   variant: SvgIconVariant;
   name: string;
+  pathname: string;
 }
 
 export const NAVIGATION_MENU: NavigationMenu[] = [
   {
     variant: 'list',
     name: '목록',
+    pathname: PATH.LIST,
   },
   {
     variant: 'home',
     name: '홈',
+    pathname: PATH.HOME,
   },
   {
     variant: 'recipe',
     name: '꿀조합',
+    pathname: PATH.RECIPE,
   },
   {
     variant: 'member',
     name: '마이',
+    pathname: PATH.MEMBER,
   },
 ];
 
