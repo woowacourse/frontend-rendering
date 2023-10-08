dade3f3783a82da588e50630187552506455b17e
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-08 16:20:33 +0900

    feat: 셀럽 프로필 컴포넌트 UI 구현

---
+++ src/components/CelebProfile.tsx
@@ -0,0 +1,12 @@
+const CelebProfile = () => {
+  return (
+    <div className='flex flex-col items-center gap-2'>
+      <div className='w-16 h-16 bg-red-100 rounded-full overflow-hidden'>
+        <img src='https://yt3.googleusercontent.com/vQrdlCaT4Tx1axJtSUa1oxp2zlnRxH-oMreTwWqB-2tdNFStIOrWWw-0jwPvVCUEjm_MywltBFY=s176-c-k-c0x00ffffff-no-rj' />
+      </div>
+      <span className='text-xs'>성시경</span>
+    </div>
+  );
+};
+
+export default CelebProfile;
