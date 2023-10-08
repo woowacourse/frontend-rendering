43bbde1727a94e16238efd1a7037802b04bd33f3
nlom0218 <nlom0218@naver.com>
2023-10-08 19:18:17 +0900

    chore: nextjs 설치 및 샘플앱 세탁

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
