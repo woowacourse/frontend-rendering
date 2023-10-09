91bce80e4fae16fe62b1b5bb4e643885c034c46d
jeongwusi <jeongwusi@gmail.com>
2023-10-09 15:09:55 +0900

    ✨ feat: CafeRank 컴포넌트 구현

---
+++ app/components/cafeRank/CafeRank.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import RankCard from "../rankCard/RankCard";
+
+type CafeRankProps = {
+  cafes: Cafe[];
+};
+
+const CafeRank = (props: CafeRankProps) => {
+  const { cafes } = props;
+
+  return (
+    <section>
+      {cafes.map((cafe) => (
+        <RankCard
+          key={cafe.id}
+          name={cafe.name}
+          address={cafe.address}
+          image={cafe.image}
+          rank={cafe.rank}
+          likeCount={cafe.likeCount}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default CafeRank;
