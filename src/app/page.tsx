c3bcf77b8c999e7d6e6a67bb80af664405875ade
GC-Park <kuyt3122@naver.com>
2023-10-09 19:47:28 +0900

    feat: TopicCard 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
-import styled from 'styled-components';
 import Button from './components/common/Button';
+import TopicCard from './components/TopicCard';
 
 export default function Page() {
   return (
     <div className="space-y-4">
-      <Button variant="primary">버튼 생성</Button>
+      <ul>
+        <TopicCard
+          name="패트릭의 맛집"
+          image="https://velog.velcdn.com/images/semnil5202/post/37f3bcb9-0b07-4100-85f6-f1d5ad037c14/image.svg"
+          creator="패트릭"
+          pinCount={6}
+          bookmarkCount={2}
+          updatedAt="2023-03-09"
+        />
+      </ul>
     </div>
   );
 }
