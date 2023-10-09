227336c39e7449a8896484d64c4b2c802e2833ca
kangyeongmin <kym6050@hanyang.ac.kr>
2023-10-09 15:15:01 +0900

    feat: 고객모드 app.tsx 파일 정의

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import styled from "@emotion/styled";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Template>
+      <Component {...pageProps} />
+    </Template>
+  );
 }
+
+const Template = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-width: 450px;
+  background: white;
+  margin: auto;
+`;
