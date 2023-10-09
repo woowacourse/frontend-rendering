71563e80a92e5bb053e5b277080fa86d1c1c3f10
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-09 20:18:30 +0900

    design: 메인페이지 UI 개선
    스크롤바 안보이게

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <main className='w-full flex flex-col gap-4'>
       <section>
         <h4>셀럽 BEST</h4>
-        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll'>
+        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll hide-scroll'>
           {celebs.map((celeb) => (
             <Link key={celeb.id} href={`/result/celeb/${celeb.name}`}>
               <CelebProfile celeb={celeb} />
@@ -27,7 +27,7 @@ export default function Home() {
 
       <section>
         <h4>셀럽잇 추천 맛집!</h4>
-        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll'>
+        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll hide-scroll'>
           {popularRestaurants.map((restaurant) => (
             <RestaurantCard
               key={restaurant.id}
@@ -40,7 +40,7 @@ export default function Home() {
 
       <section>
         <h4>어디로 가시나요?</h4>
-        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll'>
+        <div className='pl-4 pr-4 flex gap-4 overflow-x-scroll hide-scroll'>
           {Object.entries(RECOMMENDED_REGION).map((region) => (
             <Link key={region[0]} href={`/result/region/${region[0]}`}>
               <RegionIcon key={region[0]} region={region} />
