19c9e2e4ec97b6ee39cbff2121bf9abd00145b70
kangyeongmin <kym6050@hanyang.ac.kr>
2023-10-09 13:54:51 +0900

    refactor: 초기 페이지 폴더 구정 재조정

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "스탬프크러쉬",
+  description: "흩어져있는 종이쿠폰 한번에 관리하자✨",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
