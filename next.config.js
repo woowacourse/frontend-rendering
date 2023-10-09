ff7754786403761c1d553db845ab2d3241ba99de
gyeongza <gyeongza@naver.com>
2023-10-09 20:43:08 +0900

    fix: 외부 이미지 불러오도록 config 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = withVanillaExtract(nextConfig);
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           <div className={styles.logo}></div>
           <div className={styles.menuContainer}>
             <div className={styles.avatarContainer}>
-              <Avatar width={35} height={35} imageUrl="https://via.placeholder.com/150" />
+              <Avatar width={35} height={35} imageUrl="https://avatars.githubusercontent.com/u/62369936?v=4" />
             </div>
           </div>
         </div>
