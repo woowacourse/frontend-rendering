71950d39eeaec90e3505e46aab45cfa5345a7309
Q Kim <qoli0703@gmail.com>
2023-10-09 11:52:05 +0900

    chore: 이미지를 불러올 수 있는 주소 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.pium.life',
+        port: '',
+        pathname: '/**',
+      },
+      ...(process.env.NODE_ENV === 'development'
+        ? [
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/*',
+            },
+          ]
+        : []),
+    ],
+  },
 };
 
 module.exports = nextConfig;
