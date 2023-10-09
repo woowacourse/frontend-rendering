9396d2ad7912f1fbbf3c13853e21da7ff9d6ba4a
inyeong-kang <tomatorkd@naver.com>
2023-10-10 01:34:55 +0900

    chore: next/font 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import Providers from './providers';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'VoTogether',
@@ -20,7 +17,7 @@ export default function RootLayout({
       <head>
         <title>VoTogether by Next</title>
       </head>
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
