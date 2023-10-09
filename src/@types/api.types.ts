f6683d35d65f4c181dfdcca981771d7b0c3a5e92
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-09 15:57:39 +0900

    feat: fetch data 타입 정의

---
+++ src/@types/api.types.ts
@@ -0,0 +1,25 @@
+export interface RestaurantData {
+  lat: number;
+  lng: number;
+  id: number;
+  name: string;
+  category: string;
+  roadAddress: string;
+  phoneNumber: string;
+  naverMapUrl: string;
+  viewCount: number;
+  distance: number;
+  isLiked: boolean;
+  likeCount: number;
+  celebs: {
+    id: number;
+    name: string;
+    youtubeChannelName: string;
+    profileImageUrl: string;
+  }[];
+  images: { id: number; name: string; author: string; sns: string }[];
+}
+
+type Celebs = RestaurantData['celebs'];
+
+export type Celeb = Celebs[number];
