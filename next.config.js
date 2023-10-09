1989810cf0e84b990e42ba2a2727f4d1ad1afd19
jeongwusi <jeongwusi@gmail.com>
2023-10-09 15:06:13 +0900

    📦 chore: 추가 환경 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yozm.cafe",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
