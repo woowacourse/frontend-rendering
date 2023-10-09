f799f6bcdf83d01026107484a48248fea1066c88
kangyeongmin <kym6050@hanyang.ac.kr>
2023-10-09 15:35:40 +0900

    feat: 페이지 라우팅 기초 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
+import Template from "@/components/Template";
 import "@/styles/globals.css";
-import styled from "@emotion/styled";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,11 +9,3 @@ export default function App({ Component, pageProps }: AppProps) {
     </Template>
   );
 }
-
-const Template = styled.div`
-  width: 100%;
-  height: 100vh;
-  max-width: 450px;
-  background: white;
-  margin: auto;
-`;
---
+++ src/pages/reward/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const reward = () => {
+  return <div>reward</div>;
+};
+
+export default reward;
