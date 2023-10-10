761cc210d30445b092101653acb8fe90b3814036
Suyoung Jung <jsy19970512@naver.com>
2023-10-10 14:45:16 +0900

    refactor: 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import Header from '../../components/Header/Header';
-import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Header from '@/components/Header/Header';
+import NavigationBar from '@/components/NavigationBar/NavigationBar';
+import Calendar from '@/components/Calendar/Calendar';
+import styles from './page.module.css';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <main></main>
-    </>
+      <div className={styles.inner}>
+        <NavigationBar />
+        <main className={styles.main}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Calendar />
+          </Suspense>
+        </main>
+      </div>
+    </div>
   );
 }
