cb27189cd611340d402ca032e48d188d1b98e889
semnil5202 <semnil5202@gmail.com>
2023-10-10 17:24:03 +0900

    feat: Logo 컴포넌트 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Logo from './components/Logo';
+import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Logo />
+        <div className={styles.space} />
+        {children}
+      </body>
     </html>
   );
 }
