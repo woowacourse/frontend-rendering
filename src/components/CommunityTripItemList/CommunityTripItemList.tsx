4d9be29443c44f325e07bbd00b40a47ab5920f7e
dladncks1217 <dlaxodud1217@gmail.com>
2023-10-10 14:29:08 +0900

    chore: 배포 오류 수정을 위한 코드수정

--- src/components/CommunityTripItemList/CommunityTripItemList.tsx
+++ src/components/CommunityTripItemList/CommunityTripItemList.tsx
@@ -6,26 +6,26 @@ const { NEXT_PUBLIC_BASE_URL } = process.env;
 import type { CommunityTripsData } from "../../types/trips";
 
 const CommunityTripItemList = async () => {
-  const { trips }: CommunityTripsData = await fetch(
-    `${NEXT_PUBLIC_BASE_URL}/api/community-trips`,
-    {
-      cache: "force-cache",
-    }
-  )
-    .then((res) => res.json())
-    .catch(() => {
-      throw new Error("여행 목록을 불러오지 못했습니다.");
-    });
+  // const { trips }: CommunityTripsData = await fetch(
+  //   `${NEXT_PUBLIC_BASE_URL}/api/community-trips`,
+  //   {
+  //     cache: "force-cache",
+  //   }
+  // )
+  //   .then((res) => res.json())
+  //   .catch(() => {
+  //     throw new Error("여행 목록을 불러오지 못했습니다.");
+  //   });
 
   return (
     <div>
-      <section className={styles.containerStyling}>
+      {/* <section className={styles.containerStyling}>
         <ol className={styles.gridBoxStyling}>
           {trips.map((trip, index) => (
             <CommunityTripsItem key={trip.id} trip={trip} index={index} />
           ))}
         </ol>
-      </section>
+      </section> */}
     </div>
   );
 };
