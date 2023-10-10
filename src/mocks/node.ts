df447733963e8e463536f52914861cf425f76700
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-10 17:06:24 +0900

    refactor: msw 코드 제거

--- src/mocks/node.ts
+++
@@ -1,8 +0,0 @@
-import { setupServer } from 'msw/node';
-
-import { handlers } from './handlers';
-
-export const server = setupServer(...handlers);
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
