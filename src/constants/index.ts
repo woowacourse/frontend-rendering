112143c66bcecb81262e61c1a55001a00d87aee6
hae-on <solbi2004@naver.com>
2023-10-10 10:30:24 +0900

    feat: 네비게이션 바 경로에 따라 선택되도록 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -23,25 +23,39 @@ export type SvgIconVariant = (typeof SVG_ICON_VARIANTS)[number];
 
 export interface NavigationMenu {
   variant: SvgIconVariant;
-  name: string;
+  name: '검색' | '목록' | '홈' | '꿀조합' | '마이';
+  path: (typeof PATH)[keyof typeof PATH] | '/products/food' | '/products/store';
 }
+
+export const PATH = {
+  HOME: '/',
+  SEARCH: '/search',
+  PRODUCT_LIST: '/products',
+  MEMBER: '/members',
+  RECIPE: '/recipes',
+  LOGIN: '/login',
+} as const;
 
 export const NAVIGATION_MENU: NavigationMenu[] = [
   {
     variant: 'list',
     name: '목록',
+    path: `${PATH.PRODUCT_LIST}/food`,
   },
   {
     variant: 'home',
     name: '홈',
+    path: PATH.HOME,
   },
   {
     variant: 'recipe',
     name: '꿀조합',
+    path: PATH.RECIPE,
   },
   {
     variant: 'member',
     name: '마이',
+    path: PATH.MEMBER,
   },
 ];
 
