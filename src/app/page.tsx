9ff788daeff25711a66e29e00a924a0f249e6269
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-10 14:09:18 +0900

    chore: 사용하지 않는 주석 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import { styled } from 'styled-components';
-// import CarouselItem from '@/features/songs/components/CarouselItem';
-// import CollectionCarousel from '@/features/songs/components/CollectionCarousel';
 import Spacing from '@/shared/components/Spacing';
 import theme from '@/shared/styles/theme';
-// import useFetch from '@/shared/hooks/useFetch';
-// import fetcher from '@/shared/remotes';
 import type { Genre } from '@/features/songs/types/Song.type';
 import GENRES from '@/features/songs/constants/genre';
 import SongItemList from '@/features/songs/components/SongItemList';
@@ -14,27 +10,11 @@ import SongItemList from '@/features/songs/components/SongItemList';
 const genres = Object.keys(GENRES) as Genre[];
 
 const MainPage = () => {
-  // const { data: votingSongs } = useFetch<VotingSong[]>(() => fetcher('/voting-songs', 'GET'));
-
-  // if (!votingSongs) return null;
-
-  // const isEmptyVotingSongs = votingSongs.length === 0;
-
   return (
     <Container>
-      <Title>현재 킬링파트 등록중인 노래</Title>
+      <Title>현재 인기있는 노래</Title>
       <Spacing direction='vertical' size={16} />
-      {/* <CollectionCarousel>
-        {isEmptyVotingSongs ? (
-          <EmptyMessage>
-            <span>수집중인 노래가 곧 등록될 예정입니다.</span>
-          </EmptyMessage>
-        ) : (
-          votingSongs.map(votingSong => {
-            return <CarouselItem key={votingSong.id} votingSong={votingSong} />;
-          })
-        )}
-      </CollectionCarousel> */}
+
       <Spacing direction='vertical' size={24} />
       {genres.map(genre => (
         <SongItemList key={genre} genre={genre} />
