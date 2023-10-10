4261e24a49fa93273d7c2731392e68c2d1e6b2f4
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-11 08:31:21 +0900

    feat: 캐러셀 컴폰넌트 메인 페이지에 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,24 @@ import styles from './page.module.css';
 import Spacing from '@/common/Spacing';
 import { GENRES_LIST } from '@/songs/genreConstants';
 import SongItemList from '@/songs/components/SongItemList';
+import CollectionCarousel from '@/songs/components/CollectionCarousel';
+import { VotingSong } from '@/songs/song.type';
+import CarouselItem from '@/songs/components/CarouselItem';
 
-const MainPage = () => {
+const MainPage = async () => {
+  const response = await fetch(`https://dev.s-hook.com/api/voting-songs`);
+  const votingSongs = (await response.json()) as VotingSong[];
+
   return (
     <div className={styles.container}>
+      <h2 className={styles.title}>현재 등록중인 노래</h2>
+      <Spacing direction="vertical" size={16} />
+      <CollectionCarousel>
+        {votingSongs.map((votingSong) => {
+          return <CarouselItem key={votingSong.id} votingSong={votingSong} />;
+        })}
+      </CollectionCarousel>
+      <Spacing direction="vertical" size={24} />
       <h2 className={styles.title}>현재 인기있는 노래</h2>
       <Spacing direction="vertical" size={24} />
       {GENRES_LIST.map((genre) => (
