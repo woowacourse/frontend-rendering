3a7442023c6fc4472ab01a2063d91aab21adea1d
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-10 13:38:00 +0900

    feat: Header 컴포넌트 클론 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from './StyledComponentsRegistry';
+import Header from './Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ko'>
       <body className={inter.className}>
+        <Header />
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
