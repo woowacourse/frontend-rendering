a70c647cc95a3ed6b331db97e9f8f0bebce57952
nlom0218 <nlom0218@naver.com>
2023-10-10 22:16:16 +0900

    refactor: Footer 수정

--- src/components/landing/Footer/Footer.tsx
+++ src/components/landing/Footer/Footer.tsx
@@ -1,58 +1,28 @@
-import { styled } from 'styled-components';
-
-import color from '@Styles/color';
-
-import Typography from '../Typography/Typography';
+import { layout, linkContainer, typography } from './footer.css';
 
 const URL = {
   github: 'https://github.com/woowacourse-teams/2023-haru-study',
-  feedback: 'https://docs.google.com/forms/d/e/1FAIpQLSdwvz3y9xYc9PHCLw1LiaLB8TGfGao91cVs_NwERHSV9c5Mfg/viewform',
+  feedback:
+    'https://docs.google.com/forms/d/e/1FAIpQLSdwvz3y9xYc9PHCLw1LiaLB8TGfGao91cVs_NwERHSV9c5Mfg/viewform',
 };
 
 const Footer = () => {
   return (
-    <Layout>
-      <Typography variant="p3" fontSize="12px">
-        우아한테크코스 5기 하루스터디
-      </Typography>
-      <Typography variant="p3" fontSize="12px">
+    <footer className={layout}>
+      <p className={typography}>우아한테크코스 5기 하루스터디</p>
+      <p className={typography}>
         Copyright © 2023 하루스터디 - All rights reserved.
-      </Typography>
-      <LinkContainer>
-        <a target="_blank" href={URL.github} rel="noreferrer">
-          <Typography variant="p3" fontSize="12px">
-            Github
-          </Typography>
+      </p>
+      <div className={linkContainer}>
+        <a target="_blank" href={URL.github}>
+          <p className={typography}>Github</p>
         </a>
-        <a target="_blank" href={URL.feedback} rel="noreferrer">
-          <Typography variant="p3" fontSize="12px">
-            사용자 피드백
-          </Typography>
+        <a target="_blank" href={URL.feedback}>
+          <p className={typography}>사용자 피드백</p>
         </a>
-      </LinkContainer>
-    </Layout>
+      </div>
+    </footer>
   );
 };
 
 export default Footer;
-
-const Layout = styled.footer`
-  width: 100%;
-  height: 100px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const LinkContainer = styled.div`
-  display: flex;
-  gap: 20px;
-
-  margin-top: 10px;
-
-  p {
-    text-decoration: underline;
-  }
-`;
