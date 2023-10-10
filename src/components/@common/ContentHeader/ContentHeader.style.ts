089c6d612596a1c8aacdc516c968d6257b480d8a
hozzijeong <ghwls0905@naver.com>
2023-10-10 20:50:50 +0900

    feat: contentHeader 구현

---
+++ src/components/@common/ContentHeader/ContentHeader.style.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const HeaderBox = styled.header`
+	position: sticky;
+	z-index: ${({ theme: { zIndex } }) => zIndex.sticky};
+	top: 0;
+
+	display: flex;
+	align-items: center;
+
+	height: 68px;
+
+	background: ${(props) => props.theme.color.background};
+	border-bottom: solid 1px ${(props) => props.theme.color.gray};
+`;
+
+export const Title = styled.p`
+	width: 100%;
+	font: normal 2.4rem /3.2rem 'NanumSquareRound';
+	text-align: center;
+`;
