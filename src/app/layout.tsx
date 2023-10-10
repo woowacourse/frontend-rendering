9fe66b1b12feadee4dd7afcb727441997541abd5
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-10 14:33:06 +0900

    chore: 사용하지 않는 import 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from './StyledComponentsRegistry';
-import Header from './Header';
 import ContentLayout from './ContentLayout';
 
 const inter = Inter({ subsets: ['latin'] });
