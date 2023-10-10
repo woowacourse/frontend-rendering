d78eaa8021ea56838e833ad338223a57d521e4fc
hozzijeong <ghwls0905@naver.com>
2023-10-10 20:38:09 +0900

    chore: config에 이미지 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	compiler: {
+		styledComponents: true,
+	},
+	env: {
+		HOST: 'http://localhost:3000',
+	},
 
-module.exports = nextConfig
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'static.pium.life',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+				port: '',
+				pathname: '/*',
+			},
+		],
+	},
+};
+
+module.exports = nextConfig;
