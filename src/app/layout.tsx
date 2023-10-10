4f6290551d5dbab7d289c51d534d61e27441ad50
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-11 00:00:49 +0900

    feat: layout에 Header 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Header from '@/common/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 // meta. title과 description 설정 가능. server component에서만 가능한 것 같다.
 export const metadata: Metadata = {
   title: 'FE Rendering Ukko',
   description: '우코 슉 그리고 프론트엔드 렌더링',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
