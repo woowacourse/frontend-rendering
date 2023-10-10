07c518c79ac58fbda53dc987d83e014f72bd1b42
2yunseong <dbsdltjd3701@naver.com>
2023-10-10 14:43:01 +0900

    feat: header 컴포넌트 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
-import HeaderContainer from './style';
 import Image from 'next/image';
+import styled from './Header.module.css';
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <Link href='/'>
-        <div>
+    <>
+      <header className={styled.pageHeader}>
+        <Link href='/'>
           <Image
             alt='logo'
             src='/images/stampcrush_logo.svg'
             width={132}
             height={25}
           />
-        </div>
-      </Link>
-    </HeaderContainer>
+        </Link>
+      </header>
+    </>
   );
 };
 
