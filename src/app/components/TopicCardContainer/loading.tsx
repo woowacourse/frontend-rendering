638a95c1b64945c891456233ac6ca62155d3fb17
GC-Park <kuyt3122@naver.com>
2023-10-10 15:42:46 +0900

    feat: topicContainer 컴포넌트 loading 페이지 구현

---
+++ src/app/components/TopicCardContainer/loading.tsx
@@ -0,0 +1,26 @@
+import { styled } from 'styled-components';
+
+import TopicCardSkeleton from '../TopicCardSkeleton';
+
+function Loading() {
+  return (
+    <Wrapper>
+      <TopicCardSkeleton />
+      <TopicCardSkeleton />
+      <TopicCardSkeleton />
+      <TopicCardSkeleton />
+      <TopicCardSkeleton />
+      <TopicCardSkeleton />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  width: 1036px;
+  height: 300px;
+`;
+
+export default Loading;
