8c6bb716a834aff5f3dd7d1157b6ee2b4171b579
hozzijeong <ghwls0905@naver.com>
2023-10-10 13:28:58 +0900

    feat: styled-component 설정

---
+++ src/style/Global.style.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from './reset.style';
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+
+  * {
+    font-family: "NanumSquareRound", "Noto Sans KR", sans-serif;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  /********** hidden scroll **********/
+  html,
+  body {
+    scrollbar-width: none;
+    width: 100%;
+    font-size: 62.5%;
+  }
+
+  body::-webkit-scrollbar {
+    display: none;
+  }
+`;
