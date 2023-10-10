1bd0c6339d2bb2172b518d18dfa9b7cb2937da38
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-10 09:44:56 +0900

    feat: 기존 프로젝트의 theme 클론

---
+++ src/shared/styles/theme.ts
@@ -0,0 +1,42 @@
+const theme = {
+  color: {
+    primary: '#FF137F',
+    secondary: '#242424',
+    success: '#52c41a',
+    warning: '#fadb14',
+    error: '#f5222d',
+    link: '#1677ff',
+    white: '#ffffff',
+    black: '#000000',
+    black500: '#0d0d0d',
+    black400: '#1a1a1a',
+    black300: '#17171C',
+    black200: '#262626',
+    disabledBackground: '#5d5d5d',
+    disabled: '#2d2c2c',
+    subText: '#a7a7a7',
+    oauth: {
+      kakao: '#fee500',
+      google: '#ffffff',
+    },
+  },
+
+  breakPoints: {
+    xxs: '380px',
+    xs: '420px',
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    xxl: '1440px',
+  },
+
+  headerHeight: {
+    xxs: '50px',
+    mobile: '60px',
+    tablet: '80px',
+    desktop: '80px',
+  },
+} as const;
+
+export default theme;
