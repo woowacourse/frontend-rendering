25833515523294bc0473b5b51999f30e195f6f77
Youngjin Park <youngjin988@gmail.com>
2023-10-10 20:20:33 +0900

    style: css reset 추가

--- donggle-clone/src/app/ClientLayout.tsx
+++ donggle-clone/src/app/ClientLayout.tsx
@@ -1,11 +1,13 @@
 'use client';
 import StyledComponentsRegistry from '@/lib/registry';
+import GlobalStyle from '@/styles/GlobalStyle';
 import { theme } from '@/styles/theme';
 import { ThemeProvider } from 'styled-components';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <StyledComponentsRegistry>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </StyledComponentsRegistry>
   );
