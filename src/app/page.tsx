a48b1e87ea6f4544a09e62b5b501facf028b6d15
Suyoung Jung <jsy19970512@naver.com>
2023-10-10 11:45:37 +0900

    feat: Header 컴포넌트 작성

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header/Header';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+}
