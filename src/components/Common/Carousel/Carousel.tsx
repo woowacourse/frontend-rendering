ad729c3704e9fc43841ce460337cadeb5e860242
TaeeunKim <xodms0309@naver.com>
2023-10-10 15:51:34 +0900

    feat: 캐러셀 컴포넌트 추가

---
+++ src/components/Common/Carousel/Carousel.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { ReactNode, useCallback, useEffect, useState } from 'react';
+
+import styles from './carousel.module.css';
+
+interface CarouselChildren {
+  id: number;
+  children: ReactNode;
+}
+
+interface CarouselProps {
+  carouselList: CarouselChildren[];
+}
+
+const Carousel = ({ carouselList }: CarouselProps) => {
+  const extendedCarouselList = [...carouselList, carouselList[0]];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const CAROUSEL_WIDTH = window.innerWidth;
+
+  const showNextSlide = useCallback(() => {
+    setCurrentIndex((prev) => (prev === carouselList.length ? 0 : prev + 1));
+  }, [carouselList.length]);
+
+  useEffect(() => {
+    const timer = setInterval(showNextSlide, 2000);
+
+    return () => clearInterval(timer);
+  }, [showNextSlide, currentIndex]);
+
+  return (
+    <div className={styles.container}>
+      <ul
+        className={styles.carouselWrapper}
+        style={{
+          transform: 'translateX(-' + currentIndex * CAROUSEL_WIDTH + 'px)',
+          transition: currentIndex !== 0 ? 'all 0.5s ease-in-out' : '',
+        }}
+      >
+        {extendedCarouselList.map(({ id, children }, index) => (
+          <li
+            className={styles.carouselItem}
+            key={index === extendedCarouselList.length - 1 ? `${id}_last` : id}
+            style={{ width: `${CAROUSEL_WIDTH}px` }}
+          >
+            {children}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header/Header';
 export { default as NavigationBar } from './NavigationBar/NavigationBar';
 export { default as SvgIcon } from './Svg/SvgIcon';
 export { default as SvgSprite } from './Svg/SvgSprite';
+export { default as Carousel } from './Carousel/Carousel';
