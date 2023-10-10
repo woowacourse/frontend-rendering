6d4fd09a17ba04150d3c923ef8f1c64cebfc326e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-10 09:02:21 +0900

    feat: init next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '여기에 타이틀을 쓰면 보이나봐요',
+  description: 'next 탐구 중입니다.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='ko'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
