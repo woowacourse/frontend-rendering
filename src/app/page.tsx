f29752f5a4a96ac34df54046a770ba2fb0df5728
sh981013s <sh981013s@gmail.com>
2023-10-11 03:22:01 +0900

    feat: 루트 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-const Page = () => {
-  return <div>hello world!</div>;
+import RoadmapList from "@/components/roadmap/roadmapList/RoadmapList";
+
+const Home = () => {
+  return (
+    <>
+      <h1>로드맵 둘러보기</h1>
+      <RoadmapList />
+    </>
+  );
 };
 
-export default Page;
+export default Home;
