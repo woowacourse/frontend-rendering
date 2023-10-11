64eadac599c2209771d1931a6869ee7d5cf1b095
Suyoung Jung <jsy19970512@naver.com>
2023-10-11 15:21:39 +0900

    fix: 주석 처리 해제

--- src/app/api/schedules/route.ts
+++ src/app/api/schedules/route.ts
@@ -1,5 +1,6 @@
 import { schedules } from '@/mocks/fixtures/schedule';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return Response.json(schedules);
+  return NextResponse.json(schedules);
 }
