0bfd54cc40a20516020680a158770118a0d8e37f
gyeongza <gyeongza@naver.com>
2023-10-11 13:50:42 +0900

    chore: 깃허브 프로필 이미지 주소 config 추가

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
         pathname: '/**',
       },
