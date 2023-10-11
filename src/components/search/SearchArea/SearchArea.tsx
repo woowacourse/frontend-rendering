773215cbbb6614ae6ed1bde6ae873358c4908b77
bassyu <yrkdgus@gmail.com>
2023-10-11 16:09:43 +0900

    fix: 폴더구조 변경 적용

--- src/components/search/SearchArea/SearchArea.tsx
+++ src/components/search/SearchArea/SearchArea.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DictionaryPlantNameSearchResult } from "@/types";
+import { DictionaryPlantNameSearchResult } from "@/types/dictionaryPlant";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import SearchBox from "@/components/search/SearchBox/SearchBox";
