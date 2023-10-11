eb153738e97463776acbeaf3f09839b272639279
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-10-11 14:59:54 +0900

    feat: `login-kakao` 페이지 추가

---
+++ src/app/login-kakao/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div>
+      <h1>kakao 로그인 페이지</h1>
+      <Link className={`${styles["login-link"]} ${styles["--small"]}`} href='/'>
+        돌아가기
+      </Link>
+    </div>
+  );
+}
