351a1cd0430170860d7443ac26ce906d685d1a75
d0dam <smallkdb@gmail.com>
2023-10-11 15:19:06 +0900

    fix: 주석 제거

--- src/apiCall.tsx
+++ src/apiCall.tsx
@@ -1,13 +1,11 @@
 export async function getCelebs() {
-  // const res = await fetch(process.env.API_URL + "/api/celebs");
-  // const data = await res.json();
-  // return data;
-  return [];
+  const res = await fetch(process.env.API_URL + "/api/celebs");
+  const data = await res.json();
+  return data;
 }
 
 export async function getPopularRestaurants() {
-  // const res = await fetch(process.env.API_URL + "/api/popularRestaurants");
-  // const data = await res.json();
-  // return data;
-  return [];
+  const res = await fetch(process.env.API_URL + "/api/popularRestaurants");
+  const data = await res.json();
+  return data;
 }
