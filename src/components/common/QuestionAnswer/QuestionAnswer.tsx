9d8281869297f28d49567f534fb12b1b48b8a60e
woo-jk <201702042@o.cnu.ac.kr>
2023-10-11 19:57:43 +0900

    feat: QuestionAnswer, QuestionTextarea 컴포넌트 클론

---
+++ src/components/common/QuestionAnswer/QuestionAnswer.tsx
@@ -0,0 +1,60 @@
+import { css, styled } from "styled-components";
+
+import color from "@/styles/color";
+
+import CircleCheckIcon from "@/components/icons/CircleCheckIcon";
+
+import Typography from "../Typography/Typography";
+
+type Props = {
+  question: string;
+  answer?: string;
+  iconColor?: string;
+};
+
+const QuestionAnswer = ({ question, answer, iconColor }: Props) => {
+  const answerText = answer || "답변이 입력되지 않았습니다.";
+  const answerTextColor = answer ? color.black : color.neutral[400];
+  return (
+    <Layout>
+      <CircleCheckIcon circleColor={iconColor} />
+      <TypographyContainer>
+        <Typography variant="h5">{question}</Typography>
+        <Typography
+          variant="p2"
+          color={answerTextColor}
+          $style={css`
+            white-space: pre-wrap;
+          `}
+        >
+          {answerText}
+        </Typography>
+      </TypographyContainer>
+    </Layout>
+  );
+};
+
+export default QuestionAnswer;
+
+const Layout = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 12px;
+
+  justify-content: space-between;
+`;
+
+const TypographyContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  p {
+    word-break: break-all;
+  }
+
+  h5 {
+    line-height: 100%;
+  }
+`;
---
+++ src/hooks/useQuestionTextarea.ts
@@ -0,0 +1,38 @@
+import type { ChangeEventHandler } from 'react';
+import { useState } from 'react';
+
+type Parameters = {
+  minLength?: number;
+  maxLength?: number;
+  required?: boolean;
+};
+
+const useQuestionTextarea = ({ minLength, maxLength, required }: Parameters) => {
+  const [value, setValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState(required ? '* 필수입력' : '');
+
+  const validateValue = (updatedValue: string) => {
+    if (required && updatedValue.length === 0) {
+      setErrorMessage('* 필수입력');
+      return;
+    }
+
+    if (minLength && updatedValue.length < minLength) {
+      setErrorMessage(`최소 ${minLength}자 이상 입력해주세요.`);
+      return;
+    }
+
+    setErrorMessage('');
+  };
+
+  const onChange: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    const updatedValue = event.target.value;
+
+    validateValue(updatedValue);
+    setValue(updatedValue);
+  };
+
+  return { value, errorMessage, minLength, maxLength, required, onChange };
+};
+
+export default useQuestionTextarea;
