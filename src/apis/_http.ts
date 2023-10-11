1e8306c0ade1d884567a4904c26d49e4672b2258
Suyoung Jung <jsy19970512@naver.com>
2023-10-11 14:13:04 +0900

    fix: fetch url 수정

--- src/apis/_http.ts
+++ src/apis/_http.ts
@@ -2,7 +2,7 @@ const { BASE_URL } = process.env;
 
 export const http = {
   get: async <T>(endPoint: RequestInfo | URL): Promise<T> => {
-    const response = await fetch(`${BASE_URL}/api/${endPoint}`);
+    const response = await fetch(`${BASE_URL}/api${endPoint}`);
 
     if (!response.ok) {
       throw new Error('네트워크 통신 중 에러가 발생했습니다.');
