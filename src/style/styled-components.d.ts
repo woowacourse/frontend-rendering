7f3e349a3883d62cbda1f89478f7efc64f06d20f
hozzijeong <ghwls0905@naver.com>
2023-10-11 11:13:47 +0900

    chore: typescript eslint 설치 및 주석 제거

--- src/style/styled-components.d.ts
+++ src/style/styled-components.d.ts
@@ -3,6 +3,5 @@ import theme from './theme.style';
 type Theme = typeof theme;
 
 declare module 'styled-components' {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends Theme {}
+	export interface DefaultTheme extends Theme {}
 }
