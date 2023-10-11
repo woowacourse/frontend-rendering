8032782c5e178ef4c1acbd9d850a875648b3694d
hozzijeong <ghwls0905@naver.com>
2023-10-11 15:32:39 +0900

    refactor: fetch 사용 x

--- src/app/reminder/page.tsx
+++ src/app/reminder/page.tsx
@@ -3,15 +3,19 @@ import ReminderBox from '@/app/reminder/components/ReminderBox';
 import { convertReminderData } from './hooks/useReminder';
 import { Wrapper } from './ReminderPage.style';
 import ContentHeader from '@/components/@common/ContentHeader';
+import { TodayStatus } from '@/types/reminder';
 
 const getData = async () => {
 	const res = await ReminderAPI.getReminder();
 	return await res.json();
 };
 const ReminderPage = async () => {
-	const data = await getData();
-	const reminderData = convertReminderData(data);
-
+	// const data = await getData();
+	// const reminderData = convertReminderData(data);
+	const reminderData: { data: []; status: TodayStatus } = {
+		data: [],
+		status: 'late',
+	};
 	return (
 		<Wrapper $status={reminderData.status}>
 			<ContentHeader title='리마인더' />
