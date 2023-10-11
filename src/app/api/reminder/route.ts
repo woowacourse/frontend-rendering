18b09d42e1bbfc2af02fedbbdc41195cd5c554c6
hozzijeong <ghwls0905@naver.com>
2023-10-11 15:13:06 +0900

    refactor: 사용하지 않는 함수 제거

--- src/app/api/reminder/route.ts
+++ src/app/api/reminder/route.ts
@@ -1,10 +1,8 @@
 import getReminderData from '@/mock/reminderData';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export function GET() {
 	const { REMINDER_DATA } = getReminderData;
 
 	return NextResponse.json(REMINDER_DATA);
 }
-
-export async function POST() {}
