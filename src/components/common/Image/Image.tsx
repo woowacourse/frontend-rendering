b772410d73bfd8dcd91e48f0558254a8b37141b9
bassyu <yrkdgus@gmail.com>
2023-10-11 14:20:34 +0900

    feat: 이미지 컴포넌트 추가

---
+++ src/components/common/Image/Image.tsx
@@ -0,0 +1,37 @@
+import { forwardRef } from "react";
+import { StyledImage } from "./Image.style";
+
+export type ImageType = "circle" | "square" | "wide";
+
+interface ImageProps {
+  src: string;
+  alt: string;
+  size?: number;
+  type?: ImageType;
+}
+
+const Image = forwardRef<HTMLImageElement, ImageProps>(function Image(
+  props,
+  ref
+) {
+  const { size = 77, type = "circle" } = props;
+
+  // const setErrorImage: React.ReactEventHandler<HTMLImageElement> = ({
+  //   currentTarget,
+  // }) => {
+  //   currentTarget.src = "/sadpiumi.png";
+  // };
+
+  return (
+    <StyledImage
+      loading="lazy"
+      ref={ref}
+      $type={type}
+      $size={size}
+      // onError={setErrorImage}
+      {...props}
+    />
+  );
+});
+
+export default Image;
