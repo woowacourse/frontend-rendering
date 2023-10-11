a442238fbb2470be6d93ee76067539b812deefdb
hozzijeong <ghwls0905@naver.com>
2023-10-11 15:28:20 +0900

    refactor: fetch 할 수 있도록 변경

--- src/app/reminder/page.tsx
+++ src/app/reminder/page.tsx
@@ -3,19 +3,15 @@ import ReminderBox from '@/app/reminder/components/ReminderBox';
 import { convertReminderData } from './hooks/useReminder';
 import { Wrapper } from './ReminderPage.style';
 import ContentHeader from '@/components/@common/ContentHeader';
-import { TodayStatus } from '@/types/reminder';
 
 const getData = async () => {
 	const res = await ReminderAPI.getReminder();
 	return await res.json();
 };
 const ReminderPage = async () => {
-	// const data = await getData();
-	// const reminderData = convertReminderData(data);
-	const reminderData: { data: []; status: TodayStatus } = {
-		data: [],
-		status: 'late',
-	};
+	const data = await getData();
+	const reminderData = convertReminderData(data);
+
 	return (
 		<Wrapper $status={reminderData.status}>
 			<ContentHeader title='리마인더' />
