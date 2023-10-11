e58351a0c1f4e787d9824c5556634614bb448123
cruelladevil <dev.timetravel@gmail.com>
2023-10-11 15:53:58 +0900

    feat: convertTime 유틸 함수 작성

---
+++ src/app/_shared/_utils/convertTime.ts
@@ -0,0 +1,28 @@
+export type TimeMinSec = {
+  minute: number;
+  second: number;
+};
+
+export const minSecToSeconds = ({ minute, second }: TimeMinSec) => {
+  return minute * 60 + second;
+};
+
+export const secondsToMinSec = (seconds: number): TimeMinSec => {
+  return {
+    minute: Math.floor(seconds / 60),
+    second: seconds % 60,
+  };
+};
+
+export const toMinSecText = (seconds: number) => {
+  const { minute, second } = secondsToMinSec(seconds);
+
+  const minText = minute.toString().padStart(2, '0');
+  const secText = second.toString().padStart(2, '0');
+
+  return `${minText}:${secText}`;
+};
+
+export const toPlayingTimeText = (startTime: number, endTime: number) => {
+  return `${toMinSecText(startTime)} ~ ${toMinSecText(endTime)}`;
+};
