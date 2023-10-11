6db3d9e8900955c4bdf9bde58fa86ce70703f7fb
hozzijeong <ghwls0905@naver.com>
2023-10-11 16:36:19 +0900

    chore: 오타 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const NotFound = () => {
 	return (
 		<div>
-			<h2>저런, 길을 읽으셨군요</h2>
+			<h2>저런, 길을 잃으셨군요</h2>
 			<p>돌아가세요</p>
 			<Link href='/'>홈으로 가기</Link>
 		</div>
