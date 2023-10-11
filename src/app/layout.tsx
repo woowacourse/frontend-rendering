c370510440e27b819f966f870f290d003064da12
WizardRabbit <curious.wzrabbit@gmail.com>
2023-10-11 17:28:39 +0900

    ✨ feat: 레이아웃 페이지 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,46 @@
-import StyledComponentsRegistry from '../../lib/registry';
-import GlobalStyles from '../../styles/GlobalStyles';
+'use client';
 
-export default function RootLayout(props: React.PropsWithChildren) {
+import GlobalStyles from '../../styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme';
+import LandingHeader from '../../components/LandingHeader/LandingHeader';
+import '../../styles/pretendard.css';
+import { styled } from 'styled-components';
+import type { ReactNode } from 'react';
+import Head from 'next/head';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = (props: LayoutProps) => {
+  const { children } = props;
+
   return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>
-          <GlobalStyles />
-          {props.children}
-        </StyledComponentsRegistry>
-      </body>
-    </html>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <base href="/" />
+        <title>짭바팀</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Container>
+          <LandingHeader />
+          {children}
+        </Container>
+      </ThemeProvider>
+    </>
   );
-}
+};
+
+export default Layout;
