4bfda0402d875960d86205fdde8512c6a77c3b65
Suyoung Jung <jsy19970512@naver.com>
2023-10-13 14:23:50 +0900

    refactor: 불필요한 CalendarPage 제거 및 fetch 로직을 page 컴포넌트로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Header from '@/components/Header/Header';
 import NavigationBar from '@/components/NavigationBar/NavigationBar';
-import CalendarPage from '@/components/CalendarPage/CalendarPage';
+import Calendar from '@/components/Calendar/Calendar';
+import { fetchSchedules } from '@/apis/schedule';
 import styles from './page.module.css';
 
-export default function Home() {
+export default async function Home() {
+  const schedules = await fetchSchedules();
+
   return (
     <div className={styles.layout}>
       <Header />
       <div className={styles.inner}>
         <NavigationBar />
         <main className={styles.main}>
-          <CalendarPage />
+          <Calendar schedules={schedules} />
         </main>
       </div>
     </div>
