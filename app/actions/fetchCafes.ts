91471be409cb069478f54f4d180a7876f4b4de2a
jeongwusi <jeongwusi@gmail.com>
2023-10-13 14:44:42 +0900

    ♻️ refactor: fetchCafes 함수의 반환 타입 설정

--- app/actions/fetchCafes.ts
+++ app/actions/fetchCafes.ts
@@ -1,14 +1,13 @@
 "use server";
 
-const fetchCafes = async (page: number) => {
+const fetchCafes = async (page: number): Promise<Cafe[]> => {
   const apiUrl = `https://yozm.cafe/api/cafes/ranks?page=${page}`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("에러:", error);
-    return null;
+    throw error;
   }
 };
 
