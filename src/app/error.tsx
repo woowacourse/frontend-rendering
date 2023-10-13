c63430bad5258be175e53ac85d3dc5bc9acf1dc3
Suyoung Jung <jsy19970512@naver.com>
2023-10-13 14:32:02 +0900

    refactor: error 컴포넌트에 메시지 출력 및 reset 로직 추가

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,23 @@
 'use client';
 
-const Error = () => {
-  return <div>Error</div>;
+import styles from './error.module.css';
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+const Error = (props: ErrorProps) => {
+  const { error, reset } = props;
+
+  return (
+    <div className={styles.container}>
+      <h2>⛔️ {error.message}</h2>
+      <button className={styles.reset} onClick={reset}>
+        다시 시도하기
+      </button>
+    </div>
+  );
 };
 
 export default Error;
