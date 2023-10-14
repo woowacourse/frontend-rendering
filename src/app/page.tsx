4238d8c31189dea02e2eeb79bd460ff3cf83c850
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-10-14 17:55:04 +0900

    refactor: `classnames` 로 리팩터링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import classNames from "classnames/bind";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
+const cx = classNames.bind(styles);
+
 export default function Introduce() {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.logo}>
+    <div className={cx("container")}>
+      <header className={cx("header")}>
+        <div className={cx("logo")}>
           <Image
             src='/svg/donggle-logo.svg'
             alt='동글 로고'
@@ -15,16 +18,13 @@ export default function Introduce() {
           />
           동글
         </div>
-        <Link
-          className={`${styles["login-link"]} ${styles["--small-link"]}`}
-          href='/login-kakao'
-        >
+        <Link className={cx("login-link", "--small-link")} href='/login-kakao'>
           로그인하기
         </Link>
       </header>
-      <section className={styles.content}>
-        <div className={styles.introduce}>
-          <div className={styles.absolute}>
+      <section className={cx("content")}>
+        <div className={cx("introduce")}>
+          <div className={cx("absolute")}>
             <Image
               src='/svg/blur-background.svg'
               alt='백그라운드 이미지'
@@ -32,21 +32,21 @@ export default function Introduce() {
               height={800}
             />
           </div>
-          <h1 className={styles.title}>
+          <h1 className={cx("title")}>
             동글에서 블로그 글을
             <br /> 간편하게 포스팅 하세요
           </h1>
-          <p className={styles.description}>
+          <p className={cx("description")}>
             블로그 포스팅뿐만 아니라 글 관리까지 한 번에
           </p>
           <Link
-            className={`${styles["login-link"]} ${styles["--large-link"]}`}
+            className={cx("login-link", "--large-link")}
             href='/login-kakao'
           >
             동글 시작하기
           </Link>
           <Image
-            className={styles["donggle-example"]}
+            className={cx("donggle-example")}
             src='/image/donggle-example-png-2x.png'
             alt='동글 샘플'
             width={600}
