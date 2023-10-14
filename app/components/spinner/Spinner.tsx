de8f1ec2b3f275aa29a8d13e2cea5b3ab56c593b
jeongwusi <jeongwusi@gmail.com>
2023-10-14 20:59:15 +0900

    ♻️ refactor: 불필요한 코드 삭제

--- app/components/spinner/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.css";
-
-const Spinner = () => {
-  return <div className={styles.spinner} />;
-};
-
-export default Spinner;
--- app/types/index.ts
+++
@@ -1,8 +0,0 @@
-type Cafe = {
-  id?: number;
-  rank: number;
-  name: string;
-  address: string;
-  image: string;
-  likeCount: number;
-};
