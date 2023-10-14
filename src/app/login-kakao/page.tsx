33f421dc952ecc3213554e7ea84c558a4485e2f7
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-10-14 17:13:17 +0900

    refactor: LoginKakao 페이지 컴포넌트 이름 수정

--- src/app/login-kakao/page.tsx
+++ src/app/login-kakao/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 
-export default function page() {
+export default function LoginKakao() {
   return (
     <div>
       <h1>kakao 로그인 페이지</h1>
