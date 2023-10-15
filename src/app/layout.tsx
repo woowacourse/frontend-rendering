92c887a2373699671fddd6853170b5eaa08e6305
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-16 00:42:11 +0900

    fix: 기본 적용 폰트 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from './StyledComponentsRegistry';
 import ContentLayout from './ContentLayout';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '여기에 타이틀을 쓰면 보이나봐요',
@@ -14,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body className={inter.className}>
+      <body>
         <StyledComponentsRegistry>
           <ContentLayout>{children}</ContentLayout>
         </StyledComponentsRegistry>
