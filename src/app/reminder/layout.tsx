1eecfcdb3f9192ae35d5030944dbef3cdb14afae
hozzijeong <ghwls0905@naver.com>
2023-10-16 01:54:18 +0900

    chore: 사용하지 않는 메서드 제거

--- src/app/reminder/layout.tsx
+++ src/app/reminder/layout.tsx
@@ -1,6 +1,4 @@
 import Navbar from '@/components/@common/Navbar';
-import { Suspense } from 'react';
-import ReminderSpinner from './loading';
 import ContentHeader from '@/components/@common/ContentHeader';
 import { Wrapper } from './ReminderPage.style';
 
