2168a1af06545f2d47949fad83e96ec301a2ded7
cruelladevil <dev.timetravel@gmail.com>
2023-10-16 05:27:30 +0900

    fix: 의도치않게 커밋된 generateMetadata 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import styles from './page.module.css';
 import SongItemList from './_song/_components/song-item-list';
 import Spacing from './_shared/_components/spacing';
 import { Genre } from './_song/_types/song.type';
-import { Metadata } from 'next';
 
 type VotingSong = {
   id: number;
@@ -25,16 +24,6 @@ async function getVotingSongs(): Promise<VotingSong[]> {
   }
 
   return response.json();
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const votingSongsData: Promise<VotingSong[]> = getVotingSongs();
-  const votingSongs: VotingSong[] = await votingSongsData;
-
-  return {
-    title: votingSongs[0].title,
-    description: `This is the page of ${votingSongs[0].title}`,
-  };
 }
 
 export default async function Home() {
