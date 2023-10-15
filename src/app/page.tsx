7a09ac34f243b17b560b0b5dd531c0e802183a9b
woo-jk <201702042@o.cnu.ac.kr>
2023-10-15 15:14:57 +0900

    fix: 메인 페이지 error throw 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import StartSection from "@/components/landing/StartSection/StartSection";
 import Footer from "@/components/common/Footer/Footer";
 
 export default function Home() {
-  throw new Error("qwe");
   return (
     <>
       <Header />
