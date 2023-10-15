6101b39c6cb6f6dfedd09bd77215af9276c29df0
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-15 10:41:36 +0900

    style: import문 개행

--- src/common/LayoutContainer.tsx
+++ src/common/LayoutContainer.tsx
@@ -1,5 +1,6 @@
 import styles from './LayoutContainer.module.css';
 import { PropsWithChildren } from 'react';
+
 const LayoutContainer = ({ children }: PropsWithChildren) => {
   return <main className={styles.layoutContainer}>{children}</main>;
 };
