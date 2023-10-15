ed9c37ebcb4bf33598f7e0f2c201f5fc82711128
semnil5202 <semnil5202@gmail.com>
2023-10-16 01:17:37 +0900

    refactor: custom Image 방식으로 변경으로 인한 Next 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['map-befine-official.github.io'], // 이미지를 허용할 호스트 이름을 여기에 추가
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
