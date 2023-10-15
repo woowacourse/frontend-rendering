2e1e250135784b33249e5217a22ed86b5e102ebe
bassyu <yrkdgus@gmail.com>
2023-10-15 22:40:57 +0900

    feat: 에러 페이지 및 404 페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  Button,
+  ButtonSection,
+  SadPiumiImage,
+  Section,
+  Text,
+  Title,
+  Wrapper,
+} from "./error.style";
+import { useRouter } from "next/navigation";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <Wrapper>
+      <picture>
+        <SadPiumiImage
+          width={250}
+          height={160}
+          src={"/piumi-sad.png"}
+          alt="슬픈 표정을 하는 피우미"
+        />
+      </picture>
+      <Section>
+        <Text>이런!</Text>
+        <Title>오류가 생겼어요</Title>
+      </Section>
+      <Section>
+        <Text>주소가 올바른지 확인해 보세요</Text>
+        <Text>잠시 후에 다시 시도해 주세요</Text>
+      </Section>
+      <ButtonSection>
+        <Button type="button" onClick={goToHome}>
+          메인 화면으로
+        </Button>
+      </ButtonSection>
+    </Wrapper>
+  );
+};
+
+export default NotFound;
