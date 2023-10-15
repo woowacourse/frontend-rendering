b6c18eaf3c11f5b743ea64aaf092220789e48f46
bassyu <yrkdgus@gmail.com>
2023-10-15 22:33:14 +0900

    chore: 사전 식물 정보와 검색 정보 동기화

--- src/app/api/dictionary-plants/[id]/route.ts
+++ src/app/api/dictionary-plants/[id]/route.ts
@@ -5,7 +5,9 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const id = Number(params.id);
-  const data = { ...DICTIONARY_PLANT_DATA[id % 2], id };
+  const data = {
+    ...DICTIONARY_PLANT_DATA.filter((plant) => plant.id === id)[0],
+  };
 
   return Response.json(data);
 }
