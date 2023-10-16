412206281929c03f09773797c332354fe92d6ab7
turtle601 <turtle601@naver.com>
2023-10-16 14:00:04 +0900

    feat: ssr로 배포하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import getWishListData from '@/api/user';
 import RestaurantWishList from '@/components/RestaurantWishList';
 
 export default async function Home() {
-  const data = await getWishListData();
+  const res = await fetch('https://frontend-rendering-imsd.vercel.app/api', {
+    cache: 'force-cache',
+  });
+
+  const data = await res.json();
 
   return (
     <main>
