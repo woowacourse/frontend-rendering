efefcbf57d24a78bdacbc47a73b61beb28c0963c
Q Kim <qoli0703@gmail.com>
2023-10-16 14:10:33 +0900

    refactor: 로고는 서버에서 렌더링해서 보내게끔 변경

--- next.config.js
+++ next.config.js
@@ -27,6 +27,33 @@ const nextConfig = {
       },
     ],
   },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
