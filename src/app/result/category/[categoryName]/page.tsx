d3025081e786307725356c06e7421f9cc08978b8
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-16 15:12:48 +0900

    feat: 각 페이지마다 동적 메타데이터 적용

--- src/app/result/category/[categoryName]/page.tsx
+++ src/app/result/category/[categoryName]/page.tsx
@@ -1,14 +1,28 @@
 import { RestaurantData } from '@/@types/api.types';
 import { getRestaurantsByCategory } from '@/api/restaurant';
 import RestaurantCard from '@/components/server/RestaurantCard';
+import { Metadata } from 'next';
 import Link from 'next/link';
 import { use } from 'react';
 
-const ResultPage = ({
-  params: { categoryName },
-}: {
+interface Props {
   params: { categoryName: string };
-}) => {
+}
+
+export async function generateMetadata({
+  params: { categoryName },
+}: Props): Promise<Metadata> {
+  return {
+    title: `카테고리 페이지 | ${decodeURI(categoryName)}`,
+    description: `${decodeURI(categoryName)} 맛집 리스트를 확인해보세요!`,
+    openGraph: {
+      title: `${decodeURI(categoryName)} 맛집 리스트`,
+      description: `${decodeURI(categoryName)} 맛집 리스트를 확인해보세요!`,
+    },
+  };
+}
+
+const ResultPage = ({ params: { categoryName } }: Props) => {
   const Restaurants = use(getRestaurantsByCategory(categoryName)).content;
 
   return (
