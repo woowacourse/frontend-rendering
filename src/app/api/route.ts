2b33c45ed23341a5387753212b2b7de14fba318f
turtle601 <turtle601@naver.com>
2023-10-16 14:28:56 +0900

    fix: 불 필요한 파일 제거

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import restaurants from '@/mock/restaurants';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(
-    restaurants.filter((restaurant) => restaurant.isLiked)
-  );
-}
