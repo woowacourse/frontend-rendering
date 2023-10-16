83712c42cf4ee2607b82421cc906d0b9ad851a7d
turtle601 <turtle601@naver.com>
2023-10-16 14:08:15 +0900

    fix: 배포 오류 수정으로 인한 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import RestaurantWishList from '@/components/RestaurantWishList';
 
 export default async function Home() {
-  // const res = await fetch('https://frontend-rendering-imsd.vercel.app/api', {
-  //   cache: 'force-cache',
-  // });
+  const res = await fetch('https://frontend-rendering-imsd.vercel.app/api', {
+    cache: 'force-cache',
+  });
 
-  // const data = await res.json();
+  const data = await res.json();
 
   return (
     <main>
