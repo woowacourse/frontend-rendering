c0be0b2fabb0f10156545385b54223ebf741c840
고니_정우시 <jeongwusi@gmail.com>
2023-10-18 01:42:04 +0900

    [렌더링 방식의 분석] 고니(정우시) 미션 제출합니다. (#48)
    * ♻️ refactor: 불필요한 코드 삭제
    
    * 📝 docs: 렌더링 방식 탐구 및 비교 분석 작성
    
    * 📝 docs: GIF 파일 추가

--- app/components/button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-import styles from "./styles.module.css";
-
-const Button = (props: PropsWithChildren) => {
-  const { children } = props;
-  return <button className={styles.buttonStyle}>{children}</button>;
-};
-
-export default Button;
