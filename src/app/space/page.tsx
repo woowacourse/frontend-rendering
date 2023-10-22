b88dc1810fba80e209e106d728e8df2330e97349
Youngjin Park <youngjin988@gmail.com>
2023-10-22 21:21:52 +0900

    feat: 로그인 시 my-page로 이동
    `<S.Title>마이 페이지</S.Title>` 는 서버렌더링
    `<h1>{name}</h1>`는 클라이언트렌더링

--- src/app/space/page.tsx
+++ src/app/space/page.tsx
@@ -1,3 +1,68 @@
-export default function Page() {
-  return <h1>짜잔~ 내가 돌아왔다</h1>;
-}
+'use client';
+import { getMemberInfo } from 'apis/member';
+import { useMember } from 'hooks/queries/useMember';
+import { useEffect, useState } from 'react';
+import { styled } from 'styled-components';
+
+const MyPage = () => {
+  const [name, setName] = useState('');
+  // const { isLoading, name } = useMember();
+
+  useEffect(() => {
+    const getMember = async () => {
+      const member = await getMemberInfo();
+      setName(member.name);
+    };
+    getMember();
+  }, []);
+
+  return (
+    <S.Section>
+      <S.Header>
+        <S.Title>마이 페이지</S.Title>
+      </S.Header>
+      <S.Container>
+        <h1>{name}</h1>
+      </S.Container>
+    </S.Section>
+  );
+};
+
+export default MyPage;
+
+const S = {
+  Section: styled.section`
+    width: 100vw;
+    height: 100vh;
+  `,
+
+  Header: styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 4rem;
+  `,
+
+  Title: styled.h1`
+    display: flex;
+    align-items: center;
+    height: 10rem;
+    font-size: 3.2rem;
+  `,
+
+  Container: styled.div`
+    display: flex;
+    width: 100%;
+    height: calc(100% - 12rem);
+    border-top: 1px solid ${({ theme }) => theme.color.gray5};
+  `,
+
+  ContentContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8rem;
+    width: 70%;
+    padding: 4rem;
+    font-size: 1.2rem;
+  `,
+};
